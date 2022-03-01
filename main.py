c8f48d06a102e9e5e2306319a2480bcf4e425d4e
misosviso <56072239+misosviso@users.noreply.github.com>
2022-03-01 17:25:12 +0100

    Final commit

--- main.py
+++ main.py
@@ -6,22 +6,25 @@ import socketserver
 
 import sipfullproxy
 
-HOST = '0.0.0.0'
 PORT = 5060
 
 if __name__ == "__main__":
 
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+
     if len(sys.argv) < 2:
-        logging.error("Please enter valid proxy ip address")
-
+        ip_address = s.getsockname()[0]
     else:
-        logging.basicConfig(format='%(asctime)s:%(message)s', filename='calls.log', level=logging.INFO,
-                            datefmt='%H:%M:%S')
-        logging.info(time.strftime("%a, %d %b %Y %H:%M:%S ", time.localtime()))
-        hostname = socket.gethostname()
         ip_address = sys.argv[1]
-        sipfullproxy.record_route = "Record-Route: <sip:%s:%d;lr>" % (ip_address, PORT)
-        sipfullproxy.top_via = "Via: SIP/2.0/UDP %s:%d" % (ip_address, PORT)
 
-        server = socketserver.UDPServer((HOST, PORT), sipfullproxy.UDPHandler)
-        server.serve_forever()
+    logging.basicConfig(format='%(asctime)s:%(message)s', filename='calls.log', level=logging.INFO,
+                        datefmt='%H:%M:%S')
+    logging.info(time.strftime("%a, %d %b %Y %H:%M:%S ", time.localtime()))
+    hostname = socket.gethostname()
+
+    sipfullproxy.record_route = "Record-Route: <sip:%s:%d;lr>" % (ip_address, PORT)
+    sipfullproxy.top_via = "Via: SIP/2.0/UDP %s:%d" % (ip_address, PORT)
+
+    server = socketserver.UDPServer(('0.0.0.0', PORT), sipfullproxy.UDPHandler)
+    server.serve_forever()
